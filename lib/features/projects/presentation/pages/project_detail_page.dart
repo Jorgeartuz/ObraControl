80fd9a067e5ad649e13f3f8ae08f2ec049d47fc6
Jorge Artuz <artuzj1@gmail.com>
2026-09-11 00:01:14 -0500

    fix: route material exits to feature module

--- lib/features/projects/presentation/pages/project_detail_page.dart
+++ lib/features/projects/presentation/pages/project_detail_page.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import '../../../../core/database/local_database.dart';
 import 'daily_records_list_page.dart';
 import 'material_entries_list_page.dart';
-import 'material_exits_list_page.dart';
+import 'package:obrafcontrol_test/features/material_exits/presentation/pages/material_exits_list_page.dart';
 import 'machinery_list_page.dart';
 
 class ProjectDetailPage extends StatelessWidget {
